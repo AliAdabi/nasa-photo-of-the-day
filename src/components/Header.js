093856241a36ee198f52e84750ca4c81a83c8242
Aliadabi <Aliadabi3780@gmail.com>
2020-04-30 15:34:56 -0700

    MVP done

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Header (){
+    return(
+        <div className='header-container'>
+            <h1>Nasa Photo of the Day</h1>
+        </div>
+    )
+}
+export default Header;

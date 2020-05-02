f807f097894f7b3612c0bc0d800e12a096128443
Aliadabi <Aliadabi3780@gmail.com>
2020-05-02 10:06:54 -0700

    added some  styling

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React, { useState, useEffect }  from "react";
 import axios from "axios";
 import Header from './components/Header';
 import PhotoCard from './components/PhotoCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import styled from 'styled-components';
 
+
+const StyledApp = styled.div`
+  border: 3px green solid;
+  background-color: lightgreen;
+  width: 80%;
+  border-radius: 20px;
+`
+
+const StyledBox = styled.div`
+  display: flex;
+  justify-content: center;
+  border: 2px black solid;
+  width: 650px;
+  padding: 5px;
+  margin: 10px auto;
+`
 
 function App() {
   return (
+    <StyledApp>
     <div className="App">
       <Header/>
       <PhotoCard/>
     </div>
+    </StyledApp>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    h1{
+        font-size: 40px;
+        color: blue;
+    }
+    h3{
+        font-size: 32px;
+        color: blueviolet;
+    }
+`
 
 function Header (){
     return(
+        <StyledDiv>
         <div className='header-container'>
             <h1>Nasa Photo of the Day</h1>
         </div>
+        </StyledDiv>
     )
 }
 export default Header;
